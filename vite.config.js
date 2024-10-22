d0f3fdf35a0650836d122ed5488082dd058e5590
Nicolas Vazquez <vaznico93@hotmail.com>
2024-10-22 01:57:03 -0300

    aprendiendo css con los estilos y clases

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// https://vite.dev/config/
+// https://vite.dev/config/shared-options.html#css-devsourcemap
+// vite.config.js
+export default {
+    // config options
+    css: {
+        devSourcemap: true
+    }
+}
